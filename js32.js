3d2400bf7e45da6c05d2aa2e0472e8f727305e20
BILOL67980-9 <bilolsaidov728@gmail.com>
2024-09-10 16:27:38 +0500

    Update js32.js

--- js32.js
+++ js32.js
@@ -1,26 +1,26 @@
-// let count=document.querySelector('.o')
-// let input=document.querySelector('.f')
-// let pluss=document.querySelector('.f1')
-// let body=document.querySelector('body')
-
-// pluss.onclick=()=>{
-//     if(parseInt(input.values)<10){
-//         count.innerHTML='Welcom'
-//     }
-//     else{
-//         count.innerHTML="Godbye"
-//     }
-// }
 let count=document.querySelector('.o')
 let input=document.querySelector('.f')
 let pluss=document.querySelector('.f1')
 let body=document.querySelector('body')
 
 pluss.onclick=()=>{
-    if(typeof(parseInt(input.values))=='number'){
-        count.innerHTML=input.values
+    if(+input.value<10 && +input.value>=1){
+        count.innerHTML='Welcom'
     }
     else{
-        count.innerHTML='not a number'
+        count.innerHTML="Godbye"
     }
 }
+// let count=document.querySelector('.o')
+// let input=document.querySelector('.f')
+// let pluss=document.querySelector('.f1')
+// let body=document.querySelector('body')
+
+// pluss.onclick=()=>{
+//     if(typeof(parseInt(input.values))=='number'){
+//         count.innerHTML=input.values
+//     }
+//     else{
+//         count.innerHTML='not a number'
+//     }
+// }
